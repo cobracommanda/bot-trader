d15a612b53025f5ed2adcf505a1f56275417c6e3
Darrell Robinson <3dfordinner@gmail.com>
2025-10-03 09:20:49 -0400

    add auth ui and layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="auth-layout">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Signalist logo"
+            width={140}
+            height={32}
+            className="h-auto w-auto cursor-pointer"
+          />
+        </Link>
+        <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+      </section>
+      <section className="auth-right-section">
+        <div className="z-10 relative lg:mt-4 lg:mb-16">
+          <blockquote>
+            The AI didn’t just beat the market—it roasted my watchlist first,
+            highlighted the sleepers, and made me look prescient on purpose
+          </blockquote>
+          <div className="flex items-center justify-between">
+            <cite className="auth-testimonial-author">- Yolo McLovin</cite>
+            <p className="max-md:text-xs text-gray-500">Retail Investor</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-0.5">
+          {[1, 2, 3, 4, 5].map((star) => (
+            <Image
+              src="/assets/icons/star.svg"
+              alt="star"
+              key={star}
+              width={20}
+              height={20}
+              className="w-5 h-5"
+            />
+          ))}
+        </div>
+        <div className="flex-1 relative">
+          <Image
+            src="/assets/images/dashboard.png"
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Layout;
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Welcome back</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="geraldlawson@atari.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: {
+              value: /^\w+@\w+\.\w+$/,
+              message: "Please enter a valid email address",
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Signing In" : "Sign In"}
+        </Button>
+        <FooterLink
+          href="/sign-up"
+          text="Don't have an account"
+          linkText="Create an acount"
+        />
+      </form>
+    </>
+  );
+};
+export default SignIn;
